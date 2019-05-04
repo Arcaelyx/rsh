d64370daf3ded2b9c8e05739d3acc1a21e8a74b1
Arcaelyx <arcaelyx@protonmail.com>
2019-05-03 23:37:55 -0400

    Leverage directory object for accessing path metadata
    In order to be aware of the current directory at all times when
    interacting with the terminal, the path property in the directory object
    is accessed and refreshed upon every newline or EOF.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::io::Write;
 
+mod structs;
+
+use structs::Directory;
+
 fn main() {
+  let directory = Directory::new();
+  let current = directory.current.iter().last().unwrap();
   let stdin = std::io::stdin();
   let mut stdout = std::io::stdout();
   let mut buffer = String::new();
 
   loop {
-    write!(stdout, "λ ");
+    write!(stdout, "{} λ ", current.to_str().unwrap()).ok();
     buffer.clear();
     stdout.flush().ok();
     stdin.read_line(&mut buffer).ok();
