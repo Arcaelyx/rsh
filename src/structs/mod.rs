a6f075deca99f09b225436e4842200c16923881c
Arcaelyx <arcaelyx@protonmail.com>
2019-05-03 23:21:43 -0400

    Provide method for accessing path metadata
    In order to determine the system's root & current directory, a method
    has been provided. Currently the path values are being unwrapped, rather
    than being pattern matched against which will cause a kernel panic in
    the event that the paths aren't in valid UTF-8 format. This may need to
    be addressed at some future point.

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,6 +1,16 @@
+use std::env;
 use std::path::PathBuf;
 
 pub struct Directory {
   pub root: PathBuf,
   pub current: PathBuf
+}
+
+impl Directory {
+  pub fn new() -> Self {
+    Self {
+      root: env::home_dir().unwrap(),
+      current: env::current_dir().unwrap()
+    }
+  }
 }
