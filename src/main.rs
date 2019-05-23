02d8da803ce5e03cf60c7d234c22d3f68e4fd2ad
Arcaelyx <arcaelyx@protonmail.com>
2019-05-23 13:18:42 -0400

    Provide an up-to-date path when executing commands
    Given that the current directory variable is being declared outside the
    loop, whenever a path change happens, it would fail to update
    accordingly. This fixes that.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use parser::parse;
 use structs::Directory;
 
 fn main() {
-  let directory = Directory::new();
-  let current = directory.current.iter().last().unwrap();
   let stdin = std::io::stdin();
   let mut stdout = std::io::stdout();
   let mut buffer = String::new();
 
   loop {
+    let directory = Directory::new();
+    let current = directory.current.iter().last().unwrap();
     write!(stdout, "{} λ ", current.to_str().unwrap()).ok();
     buffer.clear();
     stdout.flush().ok();
